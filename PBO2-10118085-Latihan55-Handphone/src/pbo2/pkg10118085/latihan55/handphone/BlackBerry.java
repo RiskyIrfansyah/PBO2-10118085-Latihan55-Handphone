cdd25f58906cde9b41f8f9015224a531849efd50
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-11-07 19:45:37 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan55-Handphone/src/pbo2/pkg10118085/latihan55/handphone/BlackBerry.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan55.handphone;
+
+/**
+ *
+ * @author
+ * NAMA     : Risky Irfansyah
+ * KELAS    : IF-2
+ * NIM      : 10118085
+ * Deskripsi Program : Membuat program berbasis object untuk mendapatkan
+ * informasi handphone
+ */
+public class BlackBerry extends Handphone {
+    private String pinBB;
+
+    public BlackBerry(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getPinBB() {
+        return pinBB;
+    }
+
+    public void setPinBB(String pinBB) {
+        this.pinBB = pinBB;
+    }
+}
